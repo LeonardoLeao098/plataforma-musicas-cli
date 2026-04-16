a595915dd515a6c4d69ee48044f582e758ccfc51
Leonardo Barboza Leão <leo.barboza7@gmail.com>
2026-04-16 10:48:38 -0300

    feat: add band listing method

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ List<string> bandas = new List<string>();
 
 void Menu ()
 {
-    string welcome = "\nBoas vindas ao Screen Sound!";
-    Console.WriteLine(welcome);
+    Console.WriteLine("\n============================");
+    Console.WriteLine("Boas vindas ao Screen Sound!");
+    Console.WriteLine("============================");
 
     Console.WriteLine("\n[1] para registrar uma banda: ");
     Console.WriteLine("[2] para listas as bandas: ");
@@ -22,7 +23,7 @@ void Menu ()
         {
             case 1: RegistrarBanda();
                 break;
-            case 2: Console.WriteLine ("Você quer listar as bandas, beleza!");
+            case 2: MostrarBandas();
                 break;
             case 3: Console.WriteLine ("Você quer avaliar uma banda, manda a ver!");
                 break;
@@ -44,13 +45,39 @@ void RegistrarBanda()
 {
     Console.Clear();
     Console.WriteLine ("Você quer registrar uma banda, vamos lá!");
-    Console.WriteLine("Digite o nome da banda: ");
+    Console.WriteLine("\nDigite o nome da banda: ");
     string bandaNome = Console.ReadLine()!;
     bandas.Add(bandaNome);
     Console.WriteLine($"A banda {bandaNome} foi registrada com sucesso!");
-    Thread.Sleep(2000);
+    Thread.Sleep(1000);
     Console.Clear();
     Menu();
+}
+
+void MostrarBandas()
+{
+    if (bandas.Count == 0)
+    {
+        Console.Clear();
+        Console.WriteLine("Não temos bandas registradas no momento!");
+        Thread.Sleep(4000);
+        Console.Clear();
+        Menu();
+    }
+    else
+    {
+        Console.Clear();
+    Console.WriteLine("Essas são as bandas que temos no momento: \n");
+    for (int i = 0; i < bandas.Count; i++)
+    {
+        Console.WriteLine($"{bandas[i]}");
+    }
+
+    Console.WriteLine("\nAperte qualquer tecla para retornar ao menu!");
+    Console.ReadKey();
+    Console.Clear();
+    Menu();
+    }
 }
 
 Menu();
